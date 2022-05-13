98fd37427df3f9a7f2bb858152b692d9c711f4f0
ilovecl230 <1056391686@qq.com>
2022-05-13 10:57:42 +0800

    feat：布局设计

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <script>
 export default {
-  name:'app',
-  mounted(){
-    this.$storage.setItem('userInfo',{age:30,name:'jack'})
-    // this.$storage.clearItem("name")
-    // this.$storage.clearAll()
-  }
+  name:'app'
 }
 </script>
 
 <template>
+<!-- 路由的使用，从App组件开始解析，直接跳转到home路由（Home组件） -->
   <router-view></router-view>
 </template>
 
-<style>
-
+<style lang="scss">
+@import './assets/style/reset.css';
+@import './assets/style/index.scss';
 </style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,18 +1,19 @@
 <script>
 export default {
-  name:'welcome',
-  props:['msg']
+  name:'welcome'
 }
 </script>
 
 <template>
-   <div>
-    <h1>欢迎来到主页面-{{msg}}</h1>
+   <div class="welcome">
+    <h1>欢迎来到主页面</h1>
     <!-- <el-button>去登录</el-button> -->
     <router-link to="/login">去登陆</router-link>
   </div>
 </template>
 
 <style scoped>
-
+.welcome{
+  
+}
 </style>
