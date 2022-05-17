a9e668e41b0a169a9b74abc5b228d9f37e0baaa2
ilovecl230 <1056391686@qq.com>
2022-05-17 13:54:54 +0800

    feat：登录交互开发

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import request from './utils/request'
 import storage from './utils/storage'
+import api from './api'
+import store from './store'
 
 const app = createApp(App)
 app.config.globalProperties.$request = request
+app.config.globalProperties.$api = api
 app.config.globalProperties.$storage = storage
-app.use(router).use(ElementPlus).mount('#app')
+app.use(router).use(store).use(ElementPlus).mount('#app')
